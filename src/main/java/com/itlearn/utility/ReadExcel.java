b99029de5066d0956f7408fd687a2995a707c457
Chandraketu Sharma <sharma123ronit@gmail.com>
2024-11-24 15:00:55 +0530

    second commit

---
+++ src/main/java/com/itlearn/utility/ReadExcel.java
@@ -0,0 +1,85 @@
+package com.itlearn.utility;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadExcel {
+    public static FileInputStream inputStream;
+    public static XSSFWorkbook workbook;
+    public static XSSFSheet excelSheet;
+    public static XSSFRow row;
+    public static XSSFCell cell;
+
+    public static String getCellValue(String fileName, String sheetName, int rowNo, int cellNo) throws IOException {
+        try {
+            inputStream = new FileInputStream(fileName);
+            workbook = new XSSFWorkbook(inputStream);
+            excelSheet = workbook.getSheet(sheetName);
+            cell = workbook.getSheet(sheetName).getRow(rowNo).getCell(cellNo);
+            workbook.close();
+            return cell.getStringCellValue();
+        } catch (Exception e) {
+            return "";
+
+        }
+    }
+
+    public static int getRowCount(String fileName, String sheetName) {
+        try {
+            inputStream = new FileInputStream(fileName);
+            workbook = new XSSFWorkbook(inputStream);
+            excelSheet = workbook.getSheet(sheetName);
+            //get total number of row
+
+            int ttlrows = excelSheet.getLastRowNum() + 1;
+            workbook.close();
+            return ttlrows;
+
+
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public static int CellCount(String fileName, String sheetName) {
+        try {
+            inputStream = new FileInputStream(fileName);
+            workbook = new XSSFWorkbook(inputStream);
+            excelSheet = workbook.getSheet(sheetName);
+            //get total number of cell
+            int ttlcells = excelSheet.getRow(0).getLastCellNum();
+            workbook.close();
+            return ttlcells;
+
+
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    public String getStringData(int sheetIndex, int row, int column) {
+        return workbook.getSheetAt(sheetIndex).getRow(row).getCell(column).getStringCellValue();
+    }
+
+    public String getstringData(String sheetName, int row, int column) {
+        return workbook.getSheet(sheetName).getRow(row).getCell(column).getStringCellValue();
+    }
+
+
+    public double getNumericData(String sheetName, int row, int column) {
+        return workbook.getSheet(sheetName).getRow(row).getCell(column).getNumericCellValue();
+    }
+
+
+
+
+
+
+
+}
+
