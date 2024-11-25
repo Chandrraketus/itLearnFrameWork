7dbc6419feed038157ea8113291a023c1d2d464c
Chandraketu Sharma <sharma123ronit@gmail.com>
2024-11-25 22:35:44 +0530

    third commit

--- src/main/java/com/itlearn/pages/BaseTest.java
+++ src/main/java/com/itlearn/pages/BaseTest.java
@@ -1,26 +1,2 @@
-package com.itlearn.pages;
-
-import com.itlearn.utility.BrowserFactory;
-import com.itlearn.utility.ConfigDataProvider;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
-public class BaseTest {
-    public WebDriver driver;
-    public ConfigDataProvider config;
-
-    @BeforeClass
-    public void setup()
-    {
-        config = new ConfigDataProvider();
-        driver = BrowserFactory.startApplication(driver,config.getBrowser(),config.getSagingUrl());
-
-    }
-
-    @AfterClass
-    public void tearDown()
-    {
-       BrowserFactory.quitBrowser(driver);
-    }
+package com.itlearn.pages;public class BaseTest {
 }
